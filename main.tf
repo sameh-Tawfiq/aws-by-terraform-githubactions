0fe86e9cbc8641191cf8370f7fa6ff8c808c0a2a
sameh-Tawfiq <58302710+sameh-Tawfiq@users.noreply.github.com>
2022-03-27 12:38:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "vm" {
   ami           = "ami-0359b3157f016ae46"
   instance_type = "t2.micro"
   tags = {
-    Name = "my-first-tf-node"
+    Name = "my-first-tf-node2"
   }
 }
