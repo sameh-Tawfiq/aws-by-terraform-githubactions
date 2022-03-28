835eee8971599406946a173f94a07a227810ce43
sameh-Tawfiq <58302710+sameh-Tawfiq@users.noreply.github.com>
2022-03-28 09:27:00 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_cidr" {
+  description= "The VPC range"
+  type= string
+}
+
+variable "subnet1_cidr" {
+  description= "The subnet range"
+  type= string
+}
+
+variable "subnet2_cidr" {
+  description= "The subnet range"
+  type= string
+}
