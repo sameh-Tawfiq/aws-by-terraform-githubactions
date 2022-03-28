aafe730400564c03b1cdb3302363d97b6a220617
sameh-Tawfiq <58302710+sameh-Tawfiq@users.noreply.github.com>
2022-03-28 09:28:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "demosubnet1" {
   vpc_id                  = aws_vpc.demovpc.id
   cidr_block             = var.subnet1_cidr
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "Demo subnet 1"
@@ -44,7 +44,7 @@ resource "aws_subnet" "demosubnet2" {
   vpc_id                  = aws_vpc.demovpc.id
   cidr_block             = var.subnet2_cidr
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
+  availability_zone = "us-west-2b"
 
   tags = {
     Name = "Demo subnet 2"
